236fbc150f939ece83839ae3465fe38636fd9689
OmkoBass <omkob@hotmail.com>
2021-12-16 01:58:56 +0100

    Added the functionality to tamper with the blocks and revalidate them

--- app.js
+++ app.js
@@ -41,16 +41,16 @@ class Blockchain {
 
     // block.mine(this.difficulty);
     // Object.freeze ensures immutability in our code
-    this.chain.push(Object.freeze(block));
+    // this.chain.push(Object.freeze(block));
+    this.chain.push(block);
   }
 
   isValid(blockchain = this) {
-    // Iterate over the chain, we need to set i to 1 because there are nothing before the genesis block, so we start at the second block.
     for (let i = 1; i < blockchain.chain.length; i++) {
       const currentBlock = blockchain.chain[i];
       const prevBlock = blockchain.chain[i - 1];
 
-      // Check validation
+      // Check validity of the block
       if (
         currentBlock.hash !== currentBlock.getHash() ||
         prevBlock.hash !== currentBlock.prevHash
@@ -67,9 +67,19 @@ class Blockchain {
 
     const blockChain = blockchain.chain;
 
-    blockChain.map((block) => {
+    const blockChainValid = this.isValid() ? true : false;
+
+    blockChain.map((block, index) => {
       const blockDiv = document.createElement("div");
       blockDiv.classList.add("block-div");
+
+      // Changed block background color based on
+      // blockchain validity
+      if (blockChainValid) {
+        blockDiv.classList.add("block-div-valid");
+      } else {
+        blockDiv.classList.add("block-div-invalid");
+      }
 
       const blockTimestamp = document.createElement("h4");
       blockTimestamp.innerHTML = `Timestamp: ${block.timestamp}`;
@@ -80,9 +90,25 @@ class Blockchain {
       const blockPrevHash = document.createElement("h4");
       blockPrevHash.innerHTML = `PrevHash: ${block.prevHash}`;
 
+      // Add a button for every block to revalidate/remine it
+      const buttonChangeBlockValidation = document.createElement("button");
+      buttonChangeBlockValidation.textContent = "Revalidate";
+
+      buttonChangeBlockValidation.addEventListener("click", () => {
+        this.chain[index].hash = this.chain[index].getHash();
+
+        this.draw();
+      });
+
       blockDiv.append(blockTimestamp);
       blockDiv.append(blockHash);
       blockDiv.append(blockPrevHash);
+
+      // Add the validation function to every block
+      // except the first one
+      if (index > 0) {
+        blockDiv.append(buttonChangeBlockValidation);
+      }
 
       block.data.map((transaction) => {
         const transactionDiv = document.createElement("div");
@@ -91,11 +117,82 @@ class Blockchain {
         const transactionSender = document.createElement("h5");
         transactionSender.innerHTML = `Sender: ${transaction.sender}`;
 
+        // Create an input dom element
+        const transactionSenderInput = document.createElement("input");
+
+        // When we hover over the sender text
+        // it will be replaced with our input
+        transactionSender.addEventListener("mouseenter", () => {
+          if (index > 0) {
+            transactionSender.replaceWith(transactionSenderInput);
+            transactionSenderInput.value = transactionSender.innerText
+              .split(":")[1]
+              .trimStart();
+          }
+        });
+
+        // When we leave our input the text of it will be saved to the block
+        // making it tampered and invalid
+        transactionSenderInput.addEventListener("mouseleave", () => {
+          if (transactionSenderInput.value) {
+            this.chain[index].data[0].sender = transactionSenderInput.value;
+          }
+
+          transactionSenderInput.replaceWith(transactionSender);
+
+          this.draw();
+        });
+
         const transactionReceiver = document.createElement("h5");
         transactionReceiver.innerHTML = `Receiver: ${transaction.receiver}`;
 
+        // Same input logic like the sender has
+        const transactionReceiverInput = document.createElement("input");
+
+        transactionReceiver.addEventListener("mouseenter", () => {
+          if (index > 0) {
+            transactionReceiver.replaceWith(transactionReceiverInput);
+            transactionReceiverInput.value = transactionReceiver.innerText
+              .split(":")[1]
+              .trimStart();
+          }
+        });
+
+        transactionReceiverInput.addEventListener("mouseleave", () => {
+          if (transactionReceiverInput.value) {
+            this.chain[index].data[0].receiver = transactionReceiverInput.value;
+          }
+
+          transactionReceiverInput.replaceWith(transactionReceiver);
+
+          this.draw();
+        });
+
         const transactionScore = document.createElement("h5");
         transactionScore.innerHTML = `Score: ${transaction.score}`;
+
+        // Same input logic like the sender has
+        const transactionScoreInput = document.createElement("input");
+        transactionScoreInput.setAttribute("type", "number");
+
+        transactionScore.addEventListener("mouseenter", () => {
+          if (index > 0) {
+            transactionScore.replaceWith(transactionScoreInput);
+            transactionScoreInput.value = transactionScore.innerText
+              .split(":")[1]
+              .trimStart();
+          }
+        });
+
+        transactionScoreInput.addEventListener("mouseleave", () => {
+          if (transactionScoreInput.value) {
+            this.chain[index].data[0].score = transactionScoreInput.value;
+          }
+
+          transactionScoreInput.replaceWith(transactionScore);
+
+          this.draw();
+        });
 
         transactionDiv.append(transactionSender);
         transactionDiv.append(transactionReceiver);
@@ -128,7 +225,6 @@ class Block {
   getHash() {
     return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data));
   }
-
   // mine(difficulty) {
   //   // Basically, it loops until our hash starts with
   //   // the string 0...000 with length of <difficulty>.
@@ -155,8 +251,6 @@ function handleAddBlock() {
       },
     ])
   );
-
-  // console.log(blockchain.chain);
 
   senderInput.value = null;
   receiverInput.value = null;
--- bundle.js
+++ bundle.js
@@ -42,16 +42,16 @@ class Blockchain {
 
     // block.mine(this.difficulty);
     // Object.freeze ensures immutability in our code
-    this.chain.push(Object.freeze(block));
+    // this.chain.push(Object.freeze(block));
+    this.chain.push(block);
   }
 
   isValid(blockchain = this) {
-    // Iterate over the chain, we need to set i to 1 because there are nothing before the genesis block, so we start at the second block.
     for (let i = 1; i < blockchain.chain.length; i++) {
       const currentBlock = blockchain.chain[i];
       const prevBlock = blockchain.chain[i - 1];
 
-      // Check validation
+      // Check validity of the block
       if (
         currentBlock.hash !== currentBlock.getHash() ||
         prevBlock.hash !== currentBlock.prevHash
@@ -68,9 +68,19 @@ class Blockchain {
 
     const blockChain = blockchain.chain;
 
-    blockChain.map((block) => {
+    const blockChainValid = this.isValid() ? true : false;
+
+    blockChain.map((block, index) => {
       const blockDiv = document.createElement("div");
       blockDiv.classList.add("block-div");
+
+      // Changed block background color based on
+      // blockchain validity
+      if (blockChainValid) {
+        blockDiv.classList.add("block-div-valid");
+      } else {
+        blockDiv.classList.add("block-div-invalid");
+      }
 
       const blockTimestamp = document.createElement("h4");
       blockTimestamp.innerHTML = `Timestamp: ${block.timestamp}`;
@@ -81,9 +91,25 @@ class Blockchain {
       const blockPrevHash = document.createElement("h4");
       blockPrevHash.innerHTML = `PrevHash: ${block.prevHash}`;
 
+      // Add a button for every block to revalidate/remine it
+      const buttonChangeBlockValidation = document.createElement("button");
+      buttonChangeBlockValidation.textContent = "Revalidate";
+
+      buttonChangeBlockValidation.addEventListener("click", () => {
+        this.chain[index].hash = this.chain[index].getHash();
+
+        this.draw();
+      });
+
       blockDiv.append(blockTimestamp);
       blockDiv.append(blockHash);
       blockDiv.append(blockPrevHash);
+
+      // Add the validation function to every block
+      // except the first one
+      if (index > 0) {
+        blockDiv.append(buttonChangeBlockValidation);
+      }
 
       block.data.map((transaction) => {
         const transactionDiv = document.createElement("div");
@@ -92,11 +118,82 @@ class Blockchain {
         const transactionSender = document.createElement("h5");
         transactionSender.innerHTML = `Sender: ${transaction.sender}`;
 
+        // Create an input dom element
+        const transactionSenderInput = document.createElement("input");
+
+        // When we hover over the sender text
+        // it will be replaced with our input
+        transactionSender.addEventListener("mouseenter", () => {
+          if (index > 0) {
+            transactionSender.replaceWith(transactionSenderInput);
+            transactionSenderInput.value = transactionSender.innerText
+              .split(":")[1]
+              .trimStart();
+          }
+        });
+
+        // When we leave our input the text of it will be saved to the block
+        // making it tampered and invalid
+        transactionSenderInput.addEventListener("mouseleave", () => {
+          if (transactionSenderInput.value) {
+            this.chain[index].data[0].sender = transactionSenderInput.value;
+          }
+
+          transactionSenderInput.replaceWith(transactionSender);
+
+          this.draw();
+        });
+
         const transactionReceiver = document.createElement("h5");
         transactionReceiver.innerHTML = `Receiver: ${transaction.receiver}`;
 
+        // Same input logic like the sender has
+        const transactionReceiverInput = document.createElement("input");
+
+        transactionReceiver.addEventListener("mouseenter", () => {
+          if (index > 0) {
+            transactionReceiver.replaceWith(transactionReceiverInput);
+            transactionReceiverInput.value = transactionReceiver.innerText
+              .split(":")[1]
+              .trimStart();
+          }
+        });
+
+        transactionReceiverInput.addEventListener("mouseleave", () => {
+          if (transactionReceiverInput.value) {
+            this.chain[index].data[0].receiver = transactionReceiverInput.value;
+          }
+
+          transactionReceiverInput.replaceWith(transactionReceiver);
+
+          this.draw();
+        });
+
         const transactionScore = document.createElement("h5");
         transactionScore.innerHTML = `Score: ${transaction.score}`;
+
+        // Same input logic like the sender has
+        const transactionScoreInput = document.createElement("input");
+        transactionScoreInput.setAttribute("type", "number");
+
+        transactionScore.addEventListener("mouseenter", () => {
+          if (index > 0) {
+            transactionScore.replaceWith(transactionScoreInput);
+            transactionScoreInput.value = transactionScore.innerText
+              .split(":")[1]
+              .trimStart();
+          }
+        });
+
+        transactionScoreInput.addEventListener("mouseleave", () => {
+          if (transactionScoreInput.value) {
+            this.chain[index].data[0].score = transactionScoreInput.value;
+          }
+
+          transactionScoreInput.replaceWith(transactionScore);
+
+          this.draw();
+        });
 
         transactionDiv.append(transactionSender);
         transactionDiv.append(transactionReceiver);
@@ -129,7 +226,6 @@ class Block {
   getHash() {
     return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data));
   }
-
   // mine(difficulty) {
   //   // Basically, it loops until our hash starts with
   //   // the string 0...000 with length of <difficulty>.
@@ -156,8 +252,6 @@ function handleAddBlock() {
       },
     ])
   );
-
-  // console.log(blockchain.chain);
 
   senderInput.value = null;
   receiverInput.value = null;
